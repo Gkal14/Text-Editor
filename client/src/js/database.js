fa479af8c9d0e5575afa4f6dc8c0019f1cec3b42
George Kaluwin <georgekaluwin@gmail.com>
2022-09-23 16:10:54 +0930

    troubleshooting

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -20,7 +20,7 @@ export const putDb = async (content) => {
 	const store = tx.objectStore('jate');
 	const request = store.put({ id: 1, value: content });
 	const result = await request;
-	console.log("Saved to Db", result);
+	console.log(result);
 };
 
 // Retrieve text from indexedDb
